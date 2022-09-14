309b8ca69a817ec4b44a6eda251fe8eb9409d69d
KobayashiYoh <y.kobayashi@4nonome.com>
2022-09-14 14:49:11 +0900

    HomePageクラスを作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:function_trace20220914/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: const HomePage(),
     );
   }
 }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:function_trace20220914/ui_components/rounded_flat_text_button.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              const SizedBox(height: 80.0),
+              const Expanded(
+                child: Text(
+                  '機能トレース',
+                  style: TextStyle(fontSize: 48.0),
+                ),
+              ),
+              RoundedFlatTextButton(
+                onPressed: () {},
+                child: const Text('ローディング機能（setState）'),
+              ),
+              const SizedBox(height: 16.0),
+              RoundedFlatTextButton(
+                onPressed: () {},
+                child: const Text('ローディング機能（Riverpod）'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
